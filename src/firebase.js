71a7925708a542dd98754c9641c9451eae5d23f7
useon <yoosk4986@naver.com>
2022-10-10 16:42:49 +0900

    chore: firebase 기본 세팅

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
+import "firebase/compat/storage";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAkozRar5gk4wtuFxIwlxlPNkXqMunV9fs",
+  authDomain: "sentence-collection-note.firebaseapp.com",
+  projectId: "sentence-collection-note",
+  storageBucket: "sentence-collection-note.appspot.com",
+  messagingSenderId: "988105689872",
+  appId: "1:988105689872:web:b1721bcb751f1e2772b395"
+};
+
+export default firebase.initializeApp(firebaseConfig);
