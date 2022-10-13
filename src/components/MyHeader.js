a88bb5b96f30c6906205cfffb8e806a202ff4fe5
useon <yoosk4986@naver.com>
2022-10-13 17:22:41 +0900

    feat: Header 컴포넌트 구현

---
+++ src/components/MyHeader.js
@@ -0,0 +1,14 @@
+const MyHeader = ({ leftChild, rightChild }) => {
+  return (
+    <header>
+      <div className='head_btn_left'>
+        {leftChild}
+      </div>
+      <div className='head_btn_right'>
+        {rightChild}
+      </div>
+    </header>
+  )
+}
+
+export default MyHeader;
