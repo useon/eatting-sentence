41c8c35506ccf9fcf7824312c24362c90b79a45e
useon <yoosk4986@naver.com>
2022-10-16 21:12:29 +0900

    refactor: 로그인 여부에 따른 화면 전환 로직 수정

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,33 @@
-import React, { useEffect, useState } from 'react';
-import AppRouter from './Router';
-import { authService } from 'myBase'
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import '../App.css'
+import IsLogIn from '../components/IsLogIn'
+import Drawer from 'page/Drawer';
+import Home from 'page/Home';
+import NewBook from 'page/NewBook';
+import NewSentence from 'page/NewSentence';
+import EditBook from 'page/EditBook';
+import EditSentence from 'page/EditSentence';
+import SignIn from 'page/SignIn';
+
 
 function App() {
-  const [init, setInit] = useState(false);
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-  useEffect(() => {
-    authService.onAuthStateChanged((user) => {
-      if (user) {
-        setIsLoggedIn(true);
-      } else {
-        setIsLoggedIn(false);
-      }
-      setInit(true);
-    });
-  }, []);
   return (
-    <>
-      {init ? <AppRouter isLoggedIn={isLoggedIn} /> : "Initializing..."}
-    </>
+    < BrowserRouter >
+      <IsLogIn />
+      <div className='App'>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/signIn' element={<SignIn />} />
+          <Route path='/newBook' element={<NewBook />} />
+          <Route path='/newSentence' element={<NewSentence />} />
+          <Route path='/drawer/' element={<Drawer />} />
+          <Route path='/editBook/' element={<EditBook />} />
+          <Route path='/editSentence/' element={<EditSentence />} />
+        </Routes>
+      </div>
+    </ BrowserRouter>
   );
 }
 
---
+++ src/components/IsLogIn.js
@@ -0,0 +1,20 @@
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom'
+import { authService } from 'myBase';
+
+
+const IsLogIn = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    authService.onAuthStateChanged((user) => {
+      if (user) {
+        navigate('/');
+      } else {
+        navigate('/signIn');
+      }
+    });
+  }, []);
+}
+
+export default IsLogIn;
