3bdf35e3aa7e52b8720d916439ca906f2d79b7f2
useon <yoosk4986@naver.com>
2022-11-30 05:42:21 +0900

    refactor: 오타를 수정한다
    setences -> sentences

--- src/page/AddBook.js
+++ src/page/AddBook.js
@@ -12,35 +12,40 @@ const AddBook = () => {
       title: title,
       authors: authors,
       thumbnail: thumbnail,
-    })
-  }
+    });
+  };
 
   const addBookToDb = (e) => {
-    e.preventDefault()
+    e.preventDefault();
     if (window.confirm('선택한 책을 등록하시겠습니까 ?')) {
-      dbService.collection('Books').doc(bookData.title).set({
-        authors: bookData.authors.join(' '),
-        thumbnail: bookData.thumbnail,
-        setences: {},
-      })
+      dbService
+        .collection('Books')
+        .doc(bookData.title)
+        .set({
+          authors: bookData.authors.join(' '),
+          thumbnail: bookData.thumbnail,
+          sentences: {},
+        });
     }
-
-  }
+  };
 
   return (
-    <div className='AddBook'>
+    <div className="AddBook">
       <MyHeader
-        leftChild={<MyButton text={'뒤로가기'} />
-        }
+        leftChild={<MyButton text={'뒤로가기'} />}
         rightChild={
-          <button text={'완료하기'} type={'complete'} onClick={(e) => addBookToDb(e)} />
+          <button
+            text={'완료하기'}
+            type={'complete'}
+            onClick={(e) => addBookToDb(e)}
+          />
         }
       />
       <section>
         <SearchBook addBookToState={addBookToState} />
       </section>
     </div>
-  )
-}
+  );
+};
 
 export default AddBook;
