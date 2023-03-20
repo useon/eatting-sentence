f7f7a91bbe2f75036887e1c2a8b4ded6d76bab17
useon <yoosk4986@naver.com>
2023-03-20 18:47:50 +0900

    feat: 책장에서 검색시 나온 검색결과에서 클릭시 해당 책 세부페이지로 이동한다.

--- src/components/SearchModal.js
+++ src/components/SearchModal.js
@@ -1,11 +1,20 @@
-const SearchModal = ({title, authors}) => {
+import { useNavigate } from 'react-router-dom';
 
+const SearchModal = ({title, authors}) => {
+  const navigate = useNavigate();
+  const goBookPage = () => {
+    navigate(`/drawer/${title}`, {
+      state: { title: title, authors: authors},
+    });  
+  }
+  
   return (
-    <button key={title} className='search_book'>
+    <button key={title} className='search_book' onClick={goBookPage}>
       <p className='search_book title'>{title}</p>
       <p className='search_book authors'>{authors}</p>
     </button>
   )
 }
+
 
 export default SearchModal;
