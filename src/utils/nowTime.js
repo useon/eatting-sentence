fd4ed78299593355e0c1dbfa95622417367a8353
useon <yoosk4986@naver.com>
2023-04-08 22:35:49 +0900

    feat: 현재 시간을 원하는 형태로 가공하는 유틸함수를 작성한다

---
+++ src/utils/nowTime.js
@@ -0,0 +1,20 @@
+const nowTime = () => {
+  const now = new Date();
+  const year = String(now.getFullYear()).slice(2, 4);
+  let month = String(now.getMonth() + 1);
+  let date = String(now.getDate());
+  let hours = String(now.getHours());
+  let minutes = String(now.getMinutes());
+  let seconds = String(now.getSeconds());
+
+  if(Number(month) < 10) month = '0' + month;
+  if(Number(date) < 10) date = '0' + date;
+  if(Number(hours) === 12) hours = '00';
+  if(Number(minutes) < 10) minutes = '0' + minutes;
+  if(Number(seconds) < 10) seconds = '0' + seconds;
+
+  const registeredTime = Number(year + month + date + hours + minutes + seconds);
+  return registeredTime;
+}
+
+export default nowTime;
