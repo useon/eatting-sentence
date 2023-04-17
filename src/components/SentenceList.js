e78ac82195c1cf45cc3e3ad1f520e4135fd6435d
useon <yoosk4986@naver.com>
2023-04-17 23:49:20 +0900

    feat: 책 상세페이지에서 문장을 정렬해서 볼 수 있다
    최신순, 오래된순, 페이지오름차순, 페이지내림차순으로 문장을 정렬해서 볼 수 있다

--- src/components/SentenceList.js
+++ src/components/SentenceList.js
@@ -1,8 +1,11 @@
-const SentenceList = ({sentence}) => {
+const SentenceList = ({sentence, page, drawer, registeredTime}) => {
 
   return (
     <div className='sentenceList'>
+      <p>{page}</p>
       <p>{sentence}</p>
+      <p>{drawer}</p>
+      <p>{registeredTime}</p>
     </div>
   )
 }
--- src/page/Book.js
+++ src/page/Book.js
@@ -13,17 +13,63 @@ const Book = () => {
   const location = useLocation();
   const title = location.state.title;
   const authors = location.state.authors;
-  const [sentenceData, setSentenceData] = useState([]);
+  const [docSnapshot, setDocSnapshot] = useState([]);
+  const [listData, setListData] = useState([]);
+  const [senteceSorting, setSenteceSorting] = useState('페이지오름차순');
   
   useEffect(() => {
     getBookData();
-  },[])
+  },[]);
+
+  useEffect(() => {
+    reprocesser();
+  },[docSnapshot, senteceSorting]);
 
   const getBookData = async() => {
     const data = (await userDataRef.collection('sentences').where('title', '==', title).where('authors', '==', authors).get()).docs;
-    const sentence = [];
-    data.map((query) => sentence.push(query.id));
-    setSentenceData(sentence);
+    setDocSnapshot(data);
+  }
+
+  const selectHandler = (event) => {
+    setSenteceSorting(event.target.value);
+  }
+
+  const reprocesser = () => {
+    const sortedArray = [];
+    if(senteceSorting.includes('페이지')) {
+      docSnapshot.map((query) => {
+        sortedArray.push([query.data().page, [query.id, query.data().drawer, query.data().registeredTime]]);
+      })
+      sortedArray.sort((a, b) => a[0] - b[0]);
+      if(senteceSorting === '페이지오름차순')
+        setListData(sortedArray);
+      if(senteceSorting === '페이지내림차순')
+        setListData(sortedArray.reverse());
+    } else {
+      docSnapshot.map((query) => {
+        sortedArray.push([query.data().registeredTime, [query.id, query.data().page, query.data().drawer]]);
+      })
+      sortedArray.sort((a, b) => b[0] - a[0]);
+      if(senteceSorting === '최신순')
+        setListData(sortedArray);
+      if(senteceSorting === '오래된순')
+        setListData(sortedArray.reverse());
+    }
+  }
+
+  const paintSentenceList = () => {
+    const result = [];
+    if(senteceSorting.includes('페이지')) {
+      listData.map((array) => {
+        result.push(<SentenceList sentence={array[1][0]} page={array[0]} drawer={array[1][1]} registeredTime={array[1][2]}/>);
+      });
+      return result;
+    } else {
+      listData.map((array) => {
+        result.push(<SentenceList sentence={array[1][0]} page={array[1][1]} drawer={array[1][2]} registeredTime={array[0]}/>)
+      });
+      return result;
+    }
   }
 
   return (
@@ -47,6 +93,14 @@ const Book = () => {
           </div>
         }
       />
+      <div>
+        <select value={senteceSorting} onChange={selectHandler}>
+          <option value='페이지오름차순'>페이지오름차순</option>
+          <option value='페이지내림차순'>페이지내림차순</option>
+          <option value='최신순'>최신순</option>
+          <option value='오래된순'>오래된순</option>
+        </select>
+      </div>
       <section className="book">
         <div className="book information">
           <div>
@@ -55,7 +109,7 @@ const Book = () => {
           </div>
         </div>
         <div className="book sentencesWrapper">
-          {sentenceData.map((sentence) => <SentenceList sentence={sentence}/>)}
+          {paintSentenceList()}
         </div>
       </section>
     </div>
